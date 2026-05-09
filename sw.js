c06249e3e18378675ead569caea43811152983ac
zx-git-hub-cmd <user@example.com>
2026-05-09 07:56:22 +0000

    Initial commit: Wardrobe App

---
+++ sw.js
@@ -0,0 +1,45 @@
+const CACHE_NAME = 'wardrobe-app-v1';
+const ASSETS = [
+  '/index.html',
+  '/manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request).then((fetchResponse) => {
+        if (fetchResponse.status === 200) {
+          const responseClone = fetchResponse.clone();
+          caches.open(CACHE_NAME).then((cache) => {
+            cache.put(event.request, responseClone);
+          });
+        }
+        return fetchResponse;
+      });
+    }).catch(() => {
+      if (event.request.destination === 'document') {
+        return caches.match('/index.html');
+      }
+    })
+  );
+});
